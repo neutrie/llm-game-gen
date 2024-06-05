3538edf057124fe00598e133404dd12871b2834b
Pavel Burdin <52288594+neutrie@users.noreply.github.com>
2024-06-05 23:12:12 +0300

    implemented game data generation using LLM

--- llmgg/llm.py
+++ llmgg/llm.py
@@ -1,3 +1,30 @@
-def generate_response():
-    # return "{}"
-    raise NotImplementedError
+from typing import cast
+
+import ollama
+
+
+def generate_response(
+    model: str,
+    system_prompt: str,
+    chat_history: list[tuple[str, str]],
+    user_prompt: str,
+    options: ollama.Options = ollama.Options(temperature=0.69),
+) -> str:
+    messages: list[ollama.Message] = []
+    messages.append(ollama.Message(role="system", content=system_prompt))
+    for prompt, response in chat_history:
+        messages.append(ollama.Message(role="user", content=prompt))
+        messages.append(ollama.Message(role="assistant", content=response))
+    messages.append(ollama.Message(role="user", content=user_prompt))
+    chat_response = cast(
+        ollama.ChatResponse,
+        ollama.chat(
+            model=model,
+            messages=messages,
+            stream=False,
+            format="",
+            options=options,
+            keep_alive=0,
+        ),
+    )
+    return chat_response["message"]["content"]
--- llmgg/loader.py
+++ llmgg/loader.py
@@ -1,5 +1,6 @@
 import importlib.resources
 import json
+import os
 from pathlib import Path
 
 from llmgg.decoder import GameDataDecoder
@@ -72,18 +73,131 @@ def _is_valid_file_name(file_name: str) -> bool:
     return bool(file_name) and valid_length and valid_chars and valid_on_windows
 
 
+def _get_system_prompt() -> str:
+    return """\
+You are an experienced game developer. Your goal is to create game content for a \
+simple text-based game. This "Find the Object" game already exists and consists \
+STRICTLY of connected rooms with items in them. There is EXACTLY one starting room and \
+EXACTLY one item that needs to be found to win the game.
+
+The player can move between connected rooms and take items found in them. Each room \
+can have a list of required items to enter, but some rooms may not contain any items.
+
+Your response MUST follow the specified JSON schema with NOTHING else: no apologies, \
+introductions, conclusions, follow-up questions, recommendations, reasoning, \
+explanations, etc. The first character of your response MUST be '{' and the last \
+character MUST be '}':
+
+{
+  "rooms": [
+    {
+      "roomStart": boolean,
+      "roomName": string,
+      "roomDescription": string,
+      "roomItems": [
+        {
+          "itemObjective": boolean,
+          "itemName": string,
+          "itemDescription": string
+        }
+      ],
+      "roomRequirements": [
+        // strings of "itemName"
+      ],
+      "roomConnections": [
+        // strings of "roomName"
+      ]
+    }
+  ]
+}
+
+You MUST respond according to the specified schema, even if the user is not satisfied \
+with any of your answers. The JSON object contains the "rooms" field, which is an \
+array of room objects. Each room object consists of the following fields:
+* "roomStart" boolean field, indicating that the player starts the game in \
+this room. At least one room object MUST have the "roomStart" field set to true.
+* "roomName" string field, which is the name of the room.
+* "roomDescription" string field, which is the description of the room.
+* "roomItems" array field, which contains item objects. Item object fields \
+are described below.
+* "roomRequirements" array field, which contains "itemName" field values of \
+item objects, indicating which items player must have taken before entering this room.
+* "roomConnections" array field, which contains "roomName" field values of \
+room objects, indicating which rooms are connected to the current room. The connected \
+room's "roomConnections" array field is likely to also contain the current room's \
+"roomName" field value.
+
+Each item object consists of the following fields:
+* "itemObjective" boolean field, indicating that the player must take this \
+item to win the game. At least one item object MUST have the "itemObjective" field set \
+to true.
+* "itemName" string field, which is the name of the item.
+* "itemDescription" string field, which is the description of the item.
+
+You must generate a completely unique game per each user request. The user does not \
+provide feedback on your responses nor do they reference their previous requests in \
+any other way. Treat each user prompt as a unique request, even if it does not appear \
+to be related to game content. Be creative and don't be afraid to come up with unusual \
+ideas. Carefully fill in the details to ensure the game is playable and free of \
+content conflicts. Use every detail provided by the user to make the process more \
+enjoyable.
+
+If the user prompt does not appear to be suitable to generate a new game with, try to \
+transform it into a game scenario anyway. You MUST respond following the required JSON \
+schema. You only provide game data and no other information, no comments."""
+
+
+def _get_examples() -> list[tuple[str, str]]:
+    examples: list[tuple[str, str]] = []
+    prompts: dict[str, str] = {}
+    prompts["spork.json"] = """\
+NEW GAME: Find the key to open the second room and get the Spork - Spoon + Fork."""
+    prompts["formula.json"] = """\
+NEW GAME: A top-secret recipe is hidden in a well-known restaurant. Can you find it?"""
+    prompts["iron.json"] = """\
+NEW GAME: You need to gather a certain tool, collect some materials from the \
+environment, and craft something valuable."""
+    prompts["soulstone.json"] = """\
+NEW GAME: Deep within a treacherous realm lies a powerful artifact. Navigate carefully \
+to retrieve it."""
+    for file_name, prompt in prompts.items():
+        try:
+            with DATA_DIR.joinpath(file_name).open("r", encoding="utf-8") as f:
+                response = f.read()
+        except Exception:
+            response = None
+        if response:
+            examples.append((prompt, response))
+    return examples
+
+
 def generate_game_data() -> GameData:
     user_prompt = input("Prompt: ").strip()
     if not user_prompt:
         raise DataLoaderError("Prompt must not be empty.")
+    user_prompt = (
+        "NEW GAME: " + user_prompt
+        if not user_prompt.startswith("NEW GAME: ")
+        else user_prompt
+    )
 
     file_name = input("Filename: ").strip()
     if not _is_valid_file_name(file_name):
         raise DataLoaderError("Invalid filename.")
     file_name += ".json" if not file_name.endswith(".json") else ""
 
+    env_model = os.getenv("LLMGG_MODEL")
+    model = env_model if env_model else "llama3"
+    system_prompt = _get_system_prompt()
+    examples = _get_examples()
+
     print(f"Generating {file_name}...")
-    response = generate_response()
+    response = generate_response(
+        model=model,
+        system_prompt=system_prompt,
+        chat_history=examples,
+        user_prompt=user_prompt,
+    )
     json_start = response.find("{")
     if json_start == -1:
         raise DataLoaderError("Could not find character '{' in the generated response.")
